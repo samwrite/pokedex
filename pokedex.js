5792a43954efaa0f6fd357e5f3bc59191f47c246
Sam <samwrite09@yahoo.com>
2019-04-21 20:46:27 -0700

    img to gif

--- pokedex.js
+++ pokedex.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
     for(var i = 1; i <= 151; i++){
-        $("#pokemon").append("<img id='" + i + "' src='http://pokeapi.co/media/img/" + i + ".png'>");
+        $("#pokemon").append("<img id='" + i + "' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + i + ".png'>");
     }
 
     $("img").click(function(){
         var j = $(this).attr("id");
-        var entry = "http://pokeapi.co/api/v1/pokemon/" + j + "/";
+        var entry = "http://pokeapi.co/api/v2/pokemon/" + j + "/";
         $.get(entry, function(res) {
             var html_str = "";
             html_str += "<h3>" + res.name + "<h3>";
-            html_str += "<img src='http://pokeapi.co/media/img/" + j + ".png'>"
+            html_str += "<img src='http://www.pokestadium.com/sprites/black-white/animated/" + res.name + ".gif'>"
+            // html_str += "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + j + ".png'>"
             html_str += "<h4>Types</h4>";
             html_str += "<ul>"; 
             for(var i = 0; i < res.types.length; i++) {
-                html_str += "<li>" + res.types[i].name + "</li>";
+                html_str += "<li>" + res.types[i].type.name + "</li>";
             }
             html_str += "</ul>";
             html_str += "<h4>Height</h4>" + res.height;
--- selectbattle.js
+++ selectbattle.js
@@ -1,14 +1,14 @@
 var counter = 0;
 $(document).ready(function(){
     for(var i = 1; i <= 151; i++){
-        $("#pokemon").append("<img id='" + i + "' src='https:" + i + ".gif'>");
+        $("#pokemon").append("<img id='" + i + "' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + i + ".png'>");
     }
 
     //add team member
     $("#pokemon img").click(function(){
         var j = $(this).attr("id");
         if (counter < 6) {
-            $("#team").append("<img src='http://pokeapi.co/media/img/" + j + ".png'>");
+            $("#team").append("<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + j + ".png'>");
             counter++;
         }
         else {
@@ -21,7 +21,7 @@ $(document).ready(function(){
         var team = "";
         for (var i = 0; i < 6; i++) {
             var rand = Math.floor(Math.random() * 151) + 1;
-            team += "<img src='http://pokeapi.co/media/img/" + rand + ".png'>";
+            team += "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + rand + ".png'>";
         }
         counter = 6;
         $("#team").html(team);
